bd7efff88d97f81426b20b76fa831a6559fb05d1
Dave Nash <davidjnash2@gmail.com>
2023-04-20 17:05:17 -0500

    answered nine and ten

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -44,8 +44,8 @@ function isPositive( number ) {
   let result = false; //set default for anything smaller than 0
   if ( number >0 ){ //assign conditional
     result = true; //set result for values over 0
-  }
-  return result //answer for function
+  }//end conditional
+  return result; //answer for function
 } // end isPositive
 
 // Call the function to test each outcome (true & false) 
@@ -73,9 +73,9 @@ function find( value, array ){ //defining parameters
   for (let i=0; i<array.length; i++){
     if (value===array[i]){ //check if value matches any item in array
       result = true; //assigning true value if condition is met
-    }
-  }
-return result //end find with result
+    }//end if
+  }//end conditional
+return result; //end find with result
 }// end find
 
 console.log('find - should say true:', find( 'car', ['tire', 'fish', 'Lego', 'Jedi', 'toe', 'car']));
@@ -90,25 +90,38 @@ function isFirstLetter(letter, string) { //setting parameters for function
   let result = false; //giving result if condition is not met
     if (letter===string[0]){ //checking if first letter of string matches first parameter; string indexing
       result = true //result true if match
-    }
-return result //ending function with return of value
+    } //end conditional
+return result; //ending function with return of value
 }// end isFirstletter
 
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') ); //test
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') ); //test
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
-  let sum = 0
+function sumAll( array ) {//define function and parameters
+  let sum = 0 //set base value for sum result
+  for (i=0; i<array.length; i++){ //loop for all items in array
+    sum = sum + array[i]; //increase sum by/for each pass of loop
+  } //end conditional
   // TODO: loop to add items
-  return sum;
-}
+  return sum; //answer for function
+}// end sumAll
+console.log(sumAll([5.5, 32, 41])); //test
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
-
-
+function positiveNumbers(){//start and name function
+  let numbers = [-1, -14, -0.000001, -5.5, -32, 41,] //assign input array with values here in function, so that it does not change, as instructed
+  let result = []; //set default result, in this case an empty array for negative values
+  for (let i=0; i<numbers.length; i++){ //start loop to run through all values of numbers array
+    if (numbers[i]>0){ //conditional statement to capture positive index values 
+      result.push(numbers[i]); //push any captured positive values to resulting new array
+    } //end conditional
+  } //end for loop
+return result; //result of function
+} //end positiveNumbers function
+console.log('the result of positiveNumbers is:', positiveNumbers()); //test
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
